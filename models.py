7237249615f080530cb09dbc833fd85628a53396
KINGO KINGSLEY KAAH <kranedmk@gmail.com>
2025-06-06 10:46:43 +0100

    Update models.py

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, String, Float, DateTime, Text, ForeignKey, Boolean
-from sqlalchemy.relationship import relationship
+from sqlalchemy.orm import relationship  # Fixed import statement
 from sqlalchemy.sql import func
 from database import Base
 
